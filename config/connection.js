16d1d26466e69669fbcedf69f327322461b4ce97
Adam LaCasse <adamlacasse@outlook.com>
2018-07-17 15:40:32 -0400

    trying a direct connect to JawsDB

--- config/connection.js
+++ config/connection.js
@@ -3,16 +3,25 @@ var mysql = require('mysql');
 var connection;
 
 // add in the environment variable option for JAWSDB for heroku
-if (process.env.JAWSDB_URL) {
-	connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-	connection = mysql.createConnection({
-		host: 'localhost',
-		user: 'root',
-		password: 'root',
-		database: 'burgers_db'
-	});
-};
+// if (process.env.JAWSDB_URL) {
+// 	connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else {
+// 	connection = mysql.createConnection({
+// 		host: 'localhost',
+// 		user: 'root',
+// 		password: 'root',
+// 		database: 'burgers_db'
+// 	});
+// };
+
+// trying a direct connect to JawsDB...
+connection = mysql.createConnection({
+	host: '	tyduzbv3ggpf15sx.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+	user: 'aaaj9up2m13ghgjz',
+	password: 'qzo2acpab860s3db',
+	port: 3306,
+	database: 'ghkgqkaspn1oew5u'
+});
 
 // not strictly required, but best practice to establish connection
 // it's possible query establishes the connection
